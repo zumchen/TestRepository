069e7e704e645b83f070934a0f146a3c25d0c2bf
zumthik <zumthik.l@qapitol.com>
2018-05-24 15:50:39 +0530

    commit and view the change

---
+++ JavaTrainingSession/src/OppsConcept2/BMW.java
@@ -0,0 +1,12 @@
+package OppsConcept2;
+
+public class BMW extends Car {
+	
+	public void start(){
+		System.out.println("BMW starts automatically");
+	}
+	public void security(){
+		System.out.println("BMW is secured all way");
+	}
+
+}
