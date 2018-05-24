8e8c10533437e98d5f7764b01377d15b9f86e8e6
zumthik <zumthik.l@qapitol.com>
2018-05-24 15:20:32 +0530

    Commit first and check

---
+++ JavaTrainingSession/src/OppsConcept2/USBank.java
@@ -0,0 +1,21 @@
+package OppsConcept2;
+
+public interface USBank {
+	//interface should be created as interface n not a class under package
+	//interface should not have method body, only method declaration
+	//we candeclare the variable and var are by default Static
+	//var value cannot be changed
+	
+	int minBal=100;
+	public void blanChck();
+	
+	public void debit();
+	
+	public void transfer();
+	
+	
+	
+	
+	
+
+}
---
+++ JavaTrainingSession/src/OppsConcept2/unionBank.java
@@ -0,0 +1,24 @@
+package OppsConcept2;
+
+public class unionBank implements USBank {
+	public void credit(){
+		System.out.println("union===credit");
+	}
+	public void educloan(){
+		System.out.println("union edu loan");
+	}
+	public void houseloan(){
+		System.out.println("union house loan");
+	}
+	public void debit(){
+		System.out.println("");
+	}
+	public void blanChck(){
+		System.out.println("union clank cheque");
+	}
+	
+	public void transfer() {
+		
+	}
+
+}
